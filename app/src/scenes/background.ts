aaeeb57fe3c9df4397bb66c9d50ff791c00e7a84
Nicolas Ramy <nicolas.ramy@darkelda.com>
2023-10-19 14:52:53 +0200

    Fix background movement

--- app/src/scenes/background.ts
+++ app/src/scenes/background.ts
@@ -1,6 +1,8 @@
 import { Scene } from 'phaser';
 
 export default class Background extends Scene {
+
+  backgroundImages = [];
   constructor() {
     super({
       key: 'BackgroundScene'
@@ -8,10 +10,19 @@ export default class Background extends Scene {
   }
 
   create(): void {
-    // const backgroundZone = this.add.zone(400, 300, 1280, 720);
-    const backgroundImage = this.add.image(0,512,'background/forest');
-    const backgroundImage2 = this.add.image(1024,512,'background/forest');
-    // Phaser.Display.Align.In.Center(backgroundImage, backgroundZone);
-    // Phaser.Display.Align.In.RightCenter(backgroundImage2, backgroundZone);
+    this.backgroundImages.push(this.add.image(512, 512, 'background/forest'));
+    this.backgroundImages.push(this.add.image(1536, 512, 'background/forest'));
+  }
+
+  update(): void {
+    this.backgroundImages.map((x) => x.x -=1);
+
+    if (this.backgroundImages[0].x <= -512) {
+      this.backgroundImages.shift();
+    }
+
+    if (this.backgroundImages[1].x == 768) {
+      this.backgroundImages.push(this.add.image(1792, 512, 'background/forest'));
+    }
   }
 }
